7e9455a7e12dc8e48fe9d2b779d0d765470c27ae
arooshasolomon s <npj4qa@virginia.edu>
2024-05-03 20:30:49 +0000

    This is my project 3

--- get-message.py
+++ get-message.py
@@ -4,7 +4,7 @@ import requests
 import json
 
 # Set up your SQS queue URL and boto3 client
-url = "https://sqs.us-east-1.amazonaws.com/440848399208/xxxxxxx"
+url = "https://sqs.us-east-1.amazonaws.com/440848399208/npj4qa"
 sqs = boto3.client('sqs')
 
 def delete_message(handle):
@@ -18,41 +18,61 @@ def delete_message(handle):
     except ClientError as e:
         print(e.response['Error']['Message'])
 
+message_storage = {}
+
 def get_message():
-    try:
-        # Receive message from SQS queue. Each message has two MessageAttributes: order and word
-        # You want to extract these two attributes to reassemble the message
-        response = sqs.receive_message(
-            QueueUrl=url,
-            AttributeNames=[
-                'All'
-            ],
-            MaxNumberOfMessages=1,
-            MessageAttributeNames=[
-                'All'
-            ]
-        )
-        # Check if there is a message in the queue or not
-        if "Messages" in response:
-            # extract the two message attributes you want to use as variables
-            # extract the handle for deletion later
-            order = response['Messages'][0]['MessageAttributes']['order']['StringValue']
-            word = response['Messages'][0]['MessageAttributes']['word']['StringValue']
-            handle = response['Messages'][0]['ReceiptHandle']
+    queue_message = 10 
+    
+    for values in range(queue_message):
+        try:
+            # Receive message from SQS queue. Each message has two MessageAttributes: order and word
+            # You want to extract these two attributes to reassemble the message
+            response = sqs.receive_message(
+                QueueUrl=url,
+                AttributeNames=[
+                    'All'
+                ],
+                MaxNumberOfMessages=1,
+                MessageAttributeNames=[
+                    'All'
+                ]
+            )
+            # Check if there is a message in the queue or not
+            if "Messages" in response:
+                # extract the two message attributes you want to use as variables
+                # extract the handle for deletion later
+                order = response['Messages'][0]['MessageAttributes']['order']['StringValue']
+                word = response['Messages'][0]['MessageAttributes']['word']['StringValue']
+                handle = response['Messages'][0]['ReceiptHandle']
 
-            # Print the message attributes - this is what you want to work with to reassemble the message
-            print(f"Order: {order}")
-            print(f"Word: {word}")
+                # Print the message attributes - this is what you want to work with to reassemble the message
+                #print(f"Order: {order}")
+                #print(f"Word: {word}")
 
-        # If there is no message in the queue, print a message and exit    
-        else:
-            print("No message in the queue")
-            exit(1)
-            
-    # Handle any errors that may occur connecting to SQS
-    except ClientError as e:
-        print(e.response['Error']['Message'])
+                message = {order: word}
+
+                message_storage.update(message)
+
+                delete_message(handle)
+            # If there is no message in the queue, print a message and continue    
+            else:
+                print("No message in the queue")
+                continue
+        # Handle any errors that may occur connecting to SQS
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+
+    print(message_storage)
+    
+    organized_value = dict(sorted(message_storage.items()))
+    print(organized_value)
+    
+    # fetch out the VALUES 
+    for each in organized_value:
+        final_message = organized_value[each]
+        print(final_message, end=' ')
 
 # Trigger the function
 if __name__ == "__main__":
     get_message()
+
